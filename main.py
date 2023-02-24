d3c92fd8b2a1589e98cd5bbf0cd2e3328038c8cd
rosesarer <122407957+rosesarer@users.noreply.github.com>
2023-02-24 15:52:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def add(string_0, string_1): 
-    ... # write your code here
-  
+def add(num1=str, num2=str):
+
+    if num1 == "" or num2 == "":
+        return "Invaild Operation"
     
+    if num1 is None or num2 is None:
+        return "Invaild Operation"
+    
+    result = int(num1) + int(num2)
+    
+    return result
